41ca0d85fbd68a1fe53ebd91b8d9c4e8c8f0e646
Christopher Serr <christopher.serr@gmail.com>
2019-10-31 17:28:46 +0100

    Switch to crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
-use crossterm::InputEvent;
+use crossterm::{
+    execute, input, queue, EnterAlternateScreen, Goto, Hide, InputEvent, KeyEvent,
+    LeaveAlternateScreen, Output, RawScreen, ResetColor, SetBg, SetFg, Show,
+};
 use livesplit_core::component::{
     possible_time_save, previous_segment, splits, sum_of_best, timer, title,
 };
 use livesplit_core::run::parser::composite;
 use livesplit_core::{
-    settings::SemanticColor as LSColor, HotkeySystem, Run, Segment, SharedTimer, Timer,
+    layout::{ComponentState, Layout},
+    settings::{Color, Gradient},
+    HotkeySystem, Run, Segment, SharedTimer, Timer,
 };
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::{io, thread};
-use tui::backend::CrosstermBackend;
-use tui::layout::{Constraint, Direction, Layout as TuiLayout};
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Paragraph, Row, Table, Text, Widget};
-use tui::Terminal;
-
-struct Layout {
-    timer: SharedTimer,
-    components: Components,
-}
-
-struct Components {
-    timer: timer::Component,
-    splits: splits::Component,
-    title: title::Component,
-    previous_segment: previous_segment::Component,
-    sum_of_best: sum_of_best::Component,
-    possible_time_save: possible_time_save::Component,
-}
 
 fn main() {
     let run = if let Ok(run) = File::open("splits.lss")
@@ -53,188 +39,165 @@ fn main() {
         run
     };
 
+    let mut layout = Layout::default_layout();
+
     let timer = Timer::new(run).unwrap().into_shared();
     let _hotkey_system = HotkeySystem::new(timer.clone()).ok();
 
-    let mut layout = Layout {
-        timer: timer.clone(),
-        components: Components {
-            timer: timer::Component::new(),
-            splits: splits::Component::new(),
-            title: title::Component::new(),
-            previous_segment: previous_segment::Component::new(),
-            sum_of_best: sum_of_best::Component::new(),
-            possible_time_save: possible_time_save::Component::new(),
-        },
-    };
+    let _raw = RawScreen::into_raw_mode();
 
-    let mut terminal = Terminal::new(CrosstermBackend::new()).unwrap();
-    terminal.clear().unwrap();
-    terminal.hide_cursor().unwrap();
+    let stdout = io::stdout();
+    let mut stdout = BufWriter::with_capacity(128 << 10, stdout.lock());
+    queue!(stdout, Hide, EnterAlternateScreen).unwrap();
+    stdout.flush().unwrap();
 
-    let (tx, rx) = channel();
+    let mut inputs = input().read_async();
 
-    timer.write().split_or_start();
-
-    thread::spawn(move || {
-        let input = crossterm::input().read_sync();
-        for event in input {
-            tx.send(()).unwrap();
-        }
-        tx.send(()).unwrap();
-    });
-
-    loop {
-        if let Ok(_) = rx.try_recv() {
-            break;
+    'main_loop: loop {
+        for input in inputs.by_ref() {
+            match input {
+                InputEvent::Keyboard(key) => match key {
+                    KeyEvent::Ctrl('c') | KeyEvent::Esc => break 'main_loop,
+                    KeyEvent::Char('1') => timer.write().split_or_start(),
+                    KeyEvent::Char('2') => timer.write().skip_split(),
+                    KeyEvent::Char('3') => timer.write().reset(true),
+                    KeyEvent::Char('4') => timer.write().switch_to_previous_comparison(),
+                    KeyEvent::Char('5') => timer.write().toggle_pause(),
+                    KeyEvent::Char('6') => timer.write().switch_to_next_comparison(),
+                    KeyEvent::Char('8') => timer.write().undo_split(),
+                    KeyEvent::Up => layout.scroll_up(),
+                    KeyEvent::Down => layout.scroll_down(),
+                    _ => {}
+                },
+                _ => {}
+            }
         }
 
-        draw(&mut terminal, &mut layout);
-        thread::sleep(Duration::from_millis(33));
-    }
+        queue!(stdout, Goto(0, 0)).unwrap();
 
-    terminal.clear().unwrap();
-    terminal.show_cursor().unwrap();
-}
+        let state = layout.state(&timer.read());
 
-fn map_color(color: LSColor) -> Color {
-    use livesplit_core::settings::SemanticColor::*;
-    match color {
-        AheadGainingTime => Color::Rgb(0x00, 0xCC, 0x4B),
-        AheadLosingTime => Color::Rgb(0x5C, 0xD6, 0x89),
-        BehindGainingTime => Color::Rgb(0xD6, 0x5C, 0x5C),
-        BehindLosingTime => Color::Rgb(0xCC, 0x00, 0x00),
-        BestSegment => Color::Rgb(0xFF, 0xD5, 0x00),
-        NotRunning => Color::Rgb(0x99, 0x99, 0x99),
-        Paused => Color::Rgb(0x66, 0x66, 0x66),
-        PersonalBest => Color::Rgb(0x4D, 0xA6, 0xFF),
-        Default => Color::White,
-    }
-}
-
-fn draw(t: &mut Terminal<CrosstermBackend>, layout: &mut Layout) {
-    let layout_settings = Default::default();
-    let splits_state = layout
-        .components
-        .splits
-        .state(&layout.timer.read(), &layout_settings);
-
-    t.draw(|mut f| {
-        let chunks = TuiLayout::default()
-            .margin(1)
-            .constraints(vec![
-                Constraint::Length(3),
-                Constraint::Length(splits_state.splits.len() as u16 + 3),
-                Constraint::Length(2),
-                Constraint::Length(1),
-                Constraint::Length(1),
-                Constraint::Length(1),
-            ])
-            .direction(Direction::Vertical)
-            .split(f.size());
-
-        let state = layout.components.title.state(&layout.timer.read());
-
-        let category = format!("{:^35}", state.line2.unwrap_or_default());
-        let attempts = state
-            .attempts
-            .map_or_else(|| format!("{:>35}", ""), |a| format!("{:>35}", a));
-        let category: String = category
-            .chars()
-            .zip(attempts.chars())
-            .map(|(c, a)| if a.is_whitespace() { c } else { a })
-            .collect();
-
-        Paragraph::new([Text::raw(&format!("{:^35}\n{}", state.line1, category))].iter())
-            .render(&mut f, chunks[0]);
-
-        let styles = splits_state
-            .splits
-            .iter()
-            .map(|s| {
-                if s.is_current_split {
-                    Style::default().fg(Color::Rgb(77, 166, 255))
-                } else {
-                    Style::default().fg(map_color(LSColor::Default))
+        let mut row_index = 0;
+        for component in state.components {
+            match component {
+                ComponentState::Title(c) => {
+                    write!(stdout, "{:^40}", c.line1).unwrap();
+                    row_index += 1;
+                    queue!(stdout, Goto(0, row_index)).unwrap();
+                    if let Some(line2) = c.line2 {
+                        write!(stdout, "{:^40}", line2).unwrap();
+                    }
+                    row_index += 1;
+                    queue!(stdout, Goto(0, row_index)).unwrap();
                 }
-            })
-            .collect::<Vec<_>>();
-
-        let splits = splits_state
-            .splits
-            .iter()
-            .zip(styles.into_iter())
-            .map(|(s, style)| {
-                Row::StyledData(
-                    vec![
-                        s.name.clone(),
-                        format!("{:>9}", s.columns.get(1).map_or("", |c| &c.value)),
-                        format!("{:>9}", s.columns.get(0).map_or("", |c| &c.value)),
-                    ]
-                    .into_iter(),
-                    style,
-                )
-            })
-            .collect::<Vec<_>>();
-
-        let mut labels = vec![String::from("Split")];
-
-        for label in splits_state.column_labels.unwrap_or_default().iter().rev() {
-            labels.push(format!("{:>9}", label));
+                ComponentState::Timer(mut c) => {
+                    queue!(stdout, SetFg(convert_color(c.top_color))).unwrap();
+                    c.time.push_str(&c.fraction);
+                    write!(stdout, "{:>40}", c.time).unwrap();
+                    row_index += 1;
+                    queue!(stdout, ResetColor, Goto(0, row_index)).unwrap();
+                }
+                ComponentState::KeyValue(c) => {
+                    if c.display_two_rows {
+                        row_index += 1;
+                        queue!(stdout, Goto(0, row_index)).unwrap();
+                    }
+                    if let Some(color) = c.value_color {
+                        queue!(stdout, SetFg(convert_color(color))).unwrap();
+                    }
+                    write!(stdout, "{:>40}", c.value).unwrap();
+                    if c.display_two_rows {
+                        row_index -= 1;
+                    }
+                    queue!(
+                        stdout,
+                        Goto(0, row_index),
+                        SetFg(convert_color(c.key_color.unwrap_or(state.text_color)))
+                    )
+                    .unwrap();
+                    write!(stdout, "{}", c.key).unwrap();
+                    if c.display_two_rows {
+                        row_index += 2;
+                    } else {
+                        row_index += 1;
+                    }
+                    queue!(
+                        stdout,
+                        Goto(0, row_index),
+                        SetFg(convert_color(state.text_color))
+                    )
+                    .unwrap();
+                }
+                ComponentState::Splits(c) => {
+                    if let Some(labels) = c.column_labels {
+                        let mut col = 40;
+                        for label in labels {
+                            col -= 9;
+                            queue!(stdout, Goto(col, row_index)).unwrap();
+                            write!(stdout, "{:>9}", label).unwrap();
+                        }
+                    }
+                    for split in c.splits {
+                        if split.is_current_split {
+                            queue!(stdout, SetBg(convert_gradient(c.current_split_gradient)))
+                                .unwrap();
+                        }
+                        write!(stdout, "{:>40}", "").unwrap();
+                        let mut col = 40;
+                        for column in split.columns {
+                            col -= 9;
+                            queue!(
+                                stdout,
+                                Goto(col, row_index),
+                                SetFg(convert_color(column.visual_color))
+                            )
+                            .unwrap();
+                            write!(stdout, "{:>9}", column.value).unwrap();
+                        }
+                        queue!(
+                            stdout,
+                            Goto(0, row_index),
+                            SetFg(convert_color(state.text_color))
+                        )
+                        .unwrap();
+                        write!(stdout, "{}", split.name).unwrap();
+                        if split.is_current_split {
+                            queue!(stdout, ResetColor).unwrap();
+                        }
+                        row_index += 1;
+                        queue!(
+                            stdout,
+                            Goto(0, row_index),
+                            SetFg(convert_color(state.text_color))
+                        )
+                        .unwrap();
+                    }
+                }
+                _ => {}
+            }
         }
 
-        Table::new(labels.into_iter(), splits.into_iter())
-            .header_style(Style::default().fg(Color::White))
-            .widths(&[15, 9, 9])
-            .style(Style::default().fg(Color::White))
-            .column_spacing(1)
-            .render(&mut f, chunks[1]);
+        stdout.flush().unwrap();
 
-        let state = layout
-            .components
-            .timer
-            .state(&layout.timer.read(), &layout_settings);
+        thread::sleep(Duration::from_secs(1) / 30);
+    }
 
-        Paragraph::new([Text::raw(format!("{:>32}{}", state.time, state.fraction))].iter())
-            .style(
-                Style::default()
-                    .modifier(Modifier::BOLD)
-                    .fg(map_color(state.semantic_color)),
-            )
-            .render(&mut f, chunks[2]);
-
-        let state = layout
-            .components
-            .previous_segment
-            .state(&layout.timer.read(), &layout_settings);
-
-        Paragraph::new([Text::raw(format_info_text(&state.text, &state.time))].iter())
-            .style(Style::default().fg(map_color(state.semantic_color)))
-            .render(&mut f, chunks[3]);
-
-        let state = layout.components.sum_of_best.state(&layout.timer.read());
-
-        Paragraph::new([Text::raw(format_info_text(&state.text, &state.time))].iter())
-            .style(Style::default().fg(Color::White))
-            .render(&mut f, chunks[4]);
-
-        let state = layout
-            .components
-            .possible_time_save
-            .state(&layout.timer.read());
-
-        Paragraph::new([Text::raw(format_info_text(&state.text, &state.time))].iter())
-            .style(Style::default().fg(Color::White))
-            .render(&mut f, chunks[5]);
-    })
-    .unwrap();
+    execute!(stdout, LeaveAlternateScreen, Show).unwrap();
 }
 
-fn format_info_text(text: &str, value: &str) -> String {
-    let text = format!("{:<35}", text);
-    let value = format!("{:>35}", value);
-    text.chars()
-        .zip(value.chars())
-        .map(|(t, v)| if v.is_whitespace() { t } else { v })
-        .collect()
+fn convert_gradient(gradient: Gradient) -> crossterm::Color {
+    match gradient {
+        Gradient::Transparent => unimplemented!(),
+        Gradient::Horizontal(l, _r) => convert_color(l),
+        Gradient::Vertical(t, _b) => convert_color(t),
+        Gradient::Plain(c) => convert_color(c),
+    }
+}
+
+fn convert_color(color: Color) -> crossterm::Color {
+    crossterm::Color::Rgb {
+        r: (color.rgba.color.red * 255.0).round() as u8,
+        g: (color.rgba.color.green * 255.0).round() as u8,
+        b: (color.rgba.color.blue * 255.0).round() as u8,
+    }
 }
